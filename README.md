8f0950aea8771050184271f698d109ee3081884c
haerul anwar <haerul.anwar@gmail.com>
2024-12-18 16:28:02 +0700

    add docker file

--- README.md
+++ README.md
@@ -18,6 +18,14 @@ $ go run main.go
 ```
 Now server is running at port 8080.
 
+## Run on docker
+
+```bash
+# docker
+$ docker compose up --build
+```
+you can run on [http://localhost:8080](http://localhost:8080)
+
 ### User can be used
 currently there are 2 HR and 2 vendor users. below are credential can be used:
 1. username: `HR1` password: `password`
@@ -27,7 +35,7 @@ currently there are 2 HR and 2 vendor users. below are credential can be used:
 
 ## Swagger
 
-Access swagger `http://localhost:8080/swagger`
+Access swagger [http://localhost:8080/swagger](http://localhost:8080/swagger)
 
 ## ERD
 
--- controllers/event.go
+++ controllers/event.go
@@ -17,23 +17,16 @@ import (
 // @Router /api/events [get]
 // @Security Bearer
 func GetEvents(c *fiber.Ctx) error {
-	// var events []models.Event
 	var events []models.EventWithVendorName
 	role := c.Locals("role").(string)
 	userId := uint(c.Locals("user_id").(float64))
 
 	if role == constant.HR {
-		// if err := config.DB.Where("created_by = ?", userId).Find(&events).Error; err != nil {
-		// 	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Failed to fetch events"})
-		// }
-		if err := config.DB.Model(&models.Event{}).Select("events.id, events.company_name, events.proposed_dates, events.location, events.event_name, events.status, events.remarks, events.confirmed_date, events.created_by, events.created_at, users.full_name as vendor_name").Where("events.created_by = ?", userId).Joins("JOIN users ON events.vendor_id = users.id").Scan(&events).Error; err != nil {
+		if err := config.DB.Model(&models.Event{}).Select("events.id, events.company_name, events.proposed_dates, events.location, events.event_name, events.status, events.remarks, events.confirmed_date, events.created_by, events.created_at, events.vendor_id, users.full_name as vendor_name").Where("events.created_by = ?", userId).Joins("JOIN users ON events.vendor_id = users.id").Scan(&events).Error; err != nil {
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Failed to fetch events"})
 		}
 	} else if role == constant.VENDOR {
-		// if err := config.DB.Where("vendor_id = ?", userId).Joins("JOIN users ON events.created_by = users.id").Find(&events).Error; err != nil {
-		// 	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Failed to fetch events"})
-		// }
-		if err := config.DB.Model(&models.Event{}).Select("events.id, events.company_name, events.proposed_dates, events.location, events.event_name, events.status, events.remarks, events.confirmed_date, events.created_by, events.created_at, users.full_name as vendor_name").Where("events.vendor_id = ?", userId).Joins("JOIN users ON events.vendor_id = users.id").Scan(&events).Error; err != nil {
+		if err := config.DB.Model(&models.Event{}).Select("events.id, events.company_name, events.proposed_dates, events.location, events.event_name, events.status, events.remarks, events.confirmed_date, events.created_by, events.created_at, events.vendor_id, users.full_name as vendor_name").Where("events.vendor_id = ?", userId).Joins("JOIN users ON events.vendor_id = users.id").Scan(&events).Error; err != nil {
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Failed to fetch events"})
 		}
 	}
